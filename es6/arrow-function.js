36968902ed57a30dadd03cb624ddcd0223d98c8c
yoonmi <yoons0717@gmail.com>
2021-09-02 22:56:42 +0900

    project config setting

---
+++ es6/arrow-function.js
@@ -0,0 +1,17 @@
+// ES5- 함수 선언문
+function sum(a, b) {
+    return a + b;
+}
+
+// ES5- 함수 표현식 
+var sum = function (a, b) {
+    return a + b;
+}
+
+// ES6 - 함수 표현식 
+var sum = (a, b) => {
+    return a + b
+}
+
+var sum = (a,b) => a+b;
+
